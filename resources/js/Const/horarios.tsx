ee37f5992c211cc209660aada7793b159232ecf0
Alejandroxp011 <87455252+Alejandroxp011@users.noreply.github.com>
2022-04-12 21:55:06 -0400

    Revertir uso de libreria calendario a la anterior

---
+++ resources/js/Const/horarios.tsx
@@ -0,0 +1,11 @@
+export const horarios = [
+  { label: '06:45', value: '06:45' },
+  { label: '08:15', value: '08:15' },
+  { label: '11:15', value: '11:15' },
+  { label: '12:45', value: '12:45' },
+  { label: '14:15', value: '14:15' },
+  { label: '15:45', value: '15:45' },
+  { label: '17:15', value: '17:15' },
+  { label: '18:45', value: '18:45' },
+  { label: '20:15', value: '20:15' },
+]
--- resources/js/Pages/SolicitarPage.tsx
+++ resources/js/Pages/SolicitarPage.tsx
@@ -3,27 +3,10 @@ import Welcome from '@/Jetstream/Welcome';
 import AppLayout from '@/Layouts/AppLayoutTeacher';
 import SolicitarCard from '@/Jetstream/SolicitarCard';
 import Select from 'react-select';
-import Calendar from '@/Jetstream/Calendar';
-const docentes = [
-  { label: 'Leticia Blanco', value: 'Leticia Blanco' },
-  { label: 'Vladimir Costa', value: 'Leticia Blanco' },
-  { label: 'Rosemary Torrico', value: 'Leticia Blanco' },
-  { label: 'Boris Calancha', value: 'Leticia Blanco' },
-  { label: 'Marcelo Flores', value: 'Leticia Blanco' },
-];
-const horarios = [
-  { label: '06:45', value: '06:45' },
-  { label: '08:15', value: '08:15' },
-  { label: '11:15', value: '11:15' },
-  { label: '12:45', value: '12:45' },
-  { label: '14:15', value: '14:15' },
-  { label: '15:45', value: '15:45' },
-  { label: '17:15', value: '17:15' },
-  { label: '18:45', value: '18:45' },
-  { label: '20:15', value: '20:15' },
-];
-
+import * as horarios from '../Const/horarios'
+import * as docentes from '../Const/docentesPrueba';
 export default function () {
+
   return (
     <>
       <AppLayout title="Informacion">
@@ -35,29 +18,22 @@ export default function () {
         <SolicitarCard>
           <h1 className="text-center">Solicitar Aula</h1>
           <div>
-            <p className="text-left">Nombre(s) Docente(s)</p>
-            <Select
-              options={docentes}
-              isMulti
-              placeholder="Selecciona o Busca Docentes"
-            ></Select>
+             <p className="text-left">Nombre(s) Docente(s)</p>
+             <Select options={docentes}
+             isMulti
+             placeholder="Selecciona o Busca Docentes"
+             ></Select>
           </div>
-          <div className="grid grid-flow-col auto-cols-max">
-            <div className='mr-3'>
-              <p>Fecha Inicio</p>
-              <Calendar></Calendar>
-            </div>
-            <div>
-              <p>Hora de inicio</p>
-              <Select
-                options={horarios}
-                placeholder="06:45"
-                onChange={(opt: { label: any; value: any }) =>
-                  console.log(opt.label, opt.value)
-                }
-              ></Select>
-            </div>
+          <p>Hora de inicio</p>
+          <div>
+          <Select options={horarios} 
+          placeholder="06:45"
+          onChange={(opt: { label: any; value: any; }) => console.log(opt.label, opt.value)}
+          >
+    
+          </Select>
           </div>
+
         </SolicitarCard>
       </AppLayout>
     </>
