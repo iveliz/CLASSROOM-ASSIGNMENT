72ec2c2dc3d3028370f17129b0902583c9cb781f
Serboda <sebasm.sw@gmail.com>
2022-04-12 20:10:30 -0400

    Obtener usuarios que son docentes
    Obtener los usuarios que son docentes mediante el atributo rol_usuario

---
+++ app/Http/Controllers/usuarioController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use app\Models\Usuario;
+use Illuminate\Support\Facades\Redirect; 
+class usuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia:: render('docentes',['usuarios' => Usuario::where('rol_usuario','docente')->get()]); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_usuario'=>'require',
+            'nombre_usuario'=>'require',
+            'email_usuario'=>'require',
+            'contrasenia_usuario'=>'require',
+            'token_recordado_usuario'=>'require',
+            'rol_usuario'=>'require'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $consulta = Usuario::where('rol_usuario','docente')->get();
+        return $consulta
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
