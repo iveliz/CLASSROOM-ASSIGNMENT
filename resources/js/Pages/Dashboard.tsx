27e81be8fa8cd8ec876533d2c1d5f6c811a39309
IvelizAyala <ayalaiveliz@gmail.com>
2022-04-16 18:53:21 -0400

    Titulo y descripción

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -7,12 +7,15 @@ export default function Dashboard() {
     <AppLayout
       title="Informacion"
     >
+      <div className='centered'>
+      <h1 className="text-center ...">Información</h1>
+      <h3 className="text-left ...">En esta sección encontrará información para poder usar el sistema apropiadamente </h3>
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-           <h1>Informacion</h1>
           </div>
         </div>
+      </div>
       </div>
     </AppLayout>
   );
