8d073c6c102014b5109948e1302ae5bb7bfac9db
Serboda <sebasm.sw@gmail.com>
2022-04-16 12:49:25 -0400

    Obtener docentes
    Obtener docentes por nombre o id con metodos show show2

--- app/Http/Controllers/usuarioController.php
+++ app/Http/Controllers/usuarioController.php
@@ -4,91 +4,145 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-use app\Models\Usuario;
-use Illuminate\Support\Facades\Redirect; 
+use app\Models\User;
+use App\Models\DocenteMateria;
+use app\Models\Materia;
+use Illuminate\Support\Facades\Redirect;
 class usuarioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return Inertia:: render('docentes',['usuarios' => Usuario::where('rol_usuario','docente')->get()]); 
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $docentesComun = User::select('name')->get();
+    return $docentesComun;
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'id_usuario'=>'require',
-            'nombre_usuario'=>'require',
-            'email_usuario'=>'require',
-            'contrasenia_usuario'=>'require',
-            'token_recordado_usuario'=>'require',
-            'rol_usuario'=>'require'
-        ]);
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $request->validate([
+      'id_usuario' => 'require',
+      'nombre_usuario' => 'require',
+      'email_usuario' => 'require',
+      'contrasenia_usuario' => 'require',
+      'token_recordado_usuario' => 'require',
+      'rol_usuario' => 'require',
+    ]);
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $consulta = Usuario::where('rol_usuario','docente')->get();
-        return $consulta
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  string  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $materiasNomDocente = DocenteMateria::join(
+      'users',
+      'docente_materias.id_usuario',
+      '=',
+      'users.id'
+    )
+      ->select('docente_materias.id_materia')
+      ->where('users.name', 'LIKE', $id)
+      ->get();
+    $docentesidMaterias = DocenteMateria::join(
+      'materias',
+      'docente_materias.id_materia',
+      '=',
+      'materias.id_materia'
+    )
+      ->select('docente_materias.id_usuario')
+      ->whereIn('materias.id_materia', $materiasNomDocente)
+      ->get();
+    $docentesComun = User::select('name', 'id')
+      ->whereIn('id', $docentesidMaterias)
+      ->get();
+    return $docentesComun;
+  }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show2($id)
+  {
+    $materiasNomDocente = DocenteMateria::join(
+      'users',
+      'docente_materias.id_usuario',
+      '=',
+      'users.id'
+    )
+      ->select('docente_materias.id_materia')
+      ->where('users.id', $id)
+      ->get();
+    $docentesidMaterias = DocenteMateria::join(
+      'materias',
+      'docente_materias.id_materia',
+      '=',
+      'materias.id_materia'
+    )
+      ->select('docente_materias.id_usuario')
+      ->whereIn('materias.id_materia', $materiasNomDocente)
+      ->get();
+    $docentesComun = User::select('name', 'id')
+      ->whereIn('id', $docentesidMaterias)
+      ->get();
+    return $docentesComun;
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\usuarioController;
 use Inertia\Inertia;
 
 /*
@@ -16,34 +17,39 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Auth/Login');
+  return Inertia::render('Auth/Login');
 });
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+  Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+  })->name('dashboard');
 });
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
 ])->group(function () {
-    Route::get('/solicitar', function () {
-        return Inertia::render('SolicitarPage');
-    })->name('solicitar');
+  Route::get('/solicitar', function () {
+    return Inertia::render('SolicitarPage');
+  })->name('solicitar');
 });
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
 ])->group(function () {
-    Route::get('/solicitudes', function () {
-        return Inertia::render('SolicitudesPage');
-    })->name('solicitudes');
+  Route::get('/solicitudes', function () {
+    return Inertia::render('SolicitudesPage');
+  })->name('solicitudes');
 });
+
+Route::resource('usuario', usuarioController::class)->middleware([
+  'auth:sanctum',
+  'verified',
+]);
