6b4da2c9cd1891a9504e437bc783f40075299cfa
IvelizAyala <ayalaiveliz@gmail.com>
2022-04-16 23:20:21 -0400

    Titulo y descripción correctos

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -5,17 +5,15 @@ import AppLayout from '@/Layouts/AppLayoutTeacher';
 export default function Dashboard() {
   return (
     <AppLayout
-      title="Informacion"
+      title="Información"
     >
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          <div className='centered'>
             <h1 className="text-center ">Información</h1>
-            <h3 className="text-left ">En esta sección encontrará información para poder usar el sistema apropiadamente </h3>
-          </div>
-        </div>
-      </div>
+            </div>   
+            <div className="max-w-7xl mx-auto m:px-6 my-8 ">                       
+            <h5 className="text-left ">En esta sección encontrará información para poder usar el sistema apropiadamente. </h5>
+            </div>
       </div>
     </AppLayout>
   );
