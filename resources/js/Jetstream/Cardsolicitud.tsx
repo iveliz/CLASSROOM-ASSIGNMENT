0a8f6dff5b4a9fceba2587b00190755535e51158
IvelizAyala <ayalaiveliz@gmail.com>
2022-04-19 17:57:58 -0400

    crear modal de informacion de solicitud

--- resources/js/Jetstream/Cardsolicitud.tsx
+++ resources/js/Jetstream/Cardsolicitud.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import image from '../../css/images/userImage.png';
-
+import Modal from 'react-modal';
+import { useState } from 'react';
 interface Solicitud {
   Fecha: String;
   codigo: number;
@@ -8,6 +9,32 @@ interface Solicitud {
 }
 
 export default function ({ Fecha, codigo, Materia }: Solicitud) {
+  let subtitle: any;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <div>
       <div className="card mt-3 mr-8">
@@ -23,18 +50,31 @@ export default function ({ Fecha, codigo, Materia }: Solicitud) {
             {/*<div className"></div>*/}
             <button type='button'
             className='btn colorPrimary text-white'
-            data-bs-toggle="modal"
-            data-bs-target="#modalId"
+            onClick={openModal}
             >
               Detalles
             </button>
-             
+            <Modal
+              isOpen={modalIsOpen}
+              onAfterOpen={afterOpenModal}
+              onRequestClose={closeModal}
+              style={customStyles}
+              contentLabel="Example Modal"
+            >
+              <div className="font-bold justify-content-center">
+                <h2 className="border-b-4 "> Información de solicitud </h2>
               </div>
-           </div>
+              <form className="d-flex justify-content-center space-x-4 mt-4">
+                
+              </form>
+            </Modal>
+            </div>
+          </div>
 
       </div>
-     
+
  </div>
-    
+   
   );
 }
+ {/*( =ω=)..nyaa falta cancelar e informacion */ }
