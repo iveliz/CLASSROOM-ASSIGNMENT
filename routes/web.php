881f406eeaaca66105b495b9c2014c9f8a6dfbbd
JoDemVel <josuedemetriovelizescobar@gmail.com>
2022-04-19 16:57:45 -0400

    arreglar controlador

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\usuarioController;
 use Inertia\Inertia;
 use App\Http\Controllers\materiaController;
+use App\Http\Controllers\GrupoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
