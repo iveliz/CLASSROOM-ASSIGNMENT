ca674a9dd94fb450e05296246f29a53a1d1f76a8
Alejandroxp011 <87455252+Alejandroxp011@users.noreply.github.com>
2022-04-25 19:33:22 -0400

    Añadir barra de navegación a las páginas de solicitudes

---
+++ resources/js/Jetstream/SidebarAdmin.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Check from '../Icons/Check'
+import Xsquare from '../Icons/X-square'
+import List from '../Icons/List'
+import {Navigation} from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import route from 'ziggy-js';
+import '../../css/app.css'
+import { Inertia } from '@inertiajs/inertia';
+export default function () {
+    return (
+      <>
+        <Navigation
+            // you can use your own router's api to get pathname
+            activeItemId="/management/members"
+            onSelect={({itemId}) => {
+             Inertia.visit(itemId);
+            }}
+            items={[
+              {
+                title: 'Aulas',
+                itemId: 'aulas',
+                 elemBefore: () => <List></List>,
+                // you can use your own custom Icon component as well
+                // icon is optional
+                
+              },
+            /* {
+                title: 'Registros',
+                itemId: 'registros',
+                elemBefore: () => <Check></Check>,
+              
+                
+              }, */
+            ]}
+          />
+      </>
+    );
+}
--- resources/js/Pages/SolicitudesAdmin.tsx
+++ resources/js/Pages/SolicitudesAdmin.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Welcome from '@/Jetstream/Welcome';
 import AppLayout from '@/Layouts/AppLayoutAdmin';
-import Sidebar from '@/Jetstream/Sidebar';
+import Sidebar from '@/Jetstream/SidebarAdmin';
 import { solicitudes } from '@/Const/solicitudes';
 import Cardsolicitud from '@/Jetstream/Cardsolicitud';
 import axios from 'axios';
@@ -14,20 +14,19 @@ export default function () {
 
   return (
     <AppLayout title="Informacion">
-      <div className='grid grid-cols-6 gap-4'>
-      <div className=" colorPrimary mt-6 drop-shadow-lg ">
-
-      </div>
-      <div className='col-span-5'>
-        <div className='ml-5 mt-6 '>
-       <h1 className='font-bold'>Solicitudes Admin</h1> 
+      <div className="grid grid-cols-6 gap-4">
+        <div className=" colorPrimary mt-6 drop-shadow-lg ">
+          <Sidebar></Sidebar>
 
         </div>
-  
-      </div>
+        <div className="col-span-5">
+          <div className="ml-5 mt-6 ">
+            <h1 className="font-bold">Solicitudes de Aulas</h1>
+          </div>
 
-      </div>
 
+        </div>
+      </div>
     </AppLayout>
   );
 }
