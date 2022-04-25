30ae65f99b591adb9b44f02b623e04a0d3e5917a
mary173 <mariananatv@gmail.com>
2022-04-25 14:46:34 -0400

    aumentar atributo user

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware([
   'auth:sanctum',
   config('jetstream.auth_session'),
   'verified',
+  'role:docente'
 ])->group(function () {
   Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
