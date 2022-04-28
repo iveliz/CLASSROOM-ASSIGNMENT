cb28a9fcbea11d755abe36d20d6ec48873ec3b09
JoDemVel <josuedemetriovelizescobar@gmail.com>
2022-04-27 21:01:57 -0400

    corregir uso de DB en SolicitudCuentaController

--- app/Http/Controllers/SolicitudCuentaController.php
+++ app/Http/Controllers/SolicitudCuentaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\SolicitudCuenta;
 use App\Models\MateriaSolicitada;
 use Illuminate\Http\Request;
-use DB;
+use Illuminate\Support\Facades\DB;;
 class SolicitudCuentaController extends Controller
 {
   /**
