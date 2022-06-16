6bcc7a0a1571902b59ccd5bc1cc39560efde4569
mary173 <mariananatv@gmail.com>
2022-06-16 10:52:21 -0400

    emailMejorado

---
+++ app/Mail/RecuperarContraseña.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RecuperarContraseña extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return  $this->subject('Notificación de restablecimiento de contraseña')-> markdown('mail.recuperar-contraseñal');
+    }
+}
