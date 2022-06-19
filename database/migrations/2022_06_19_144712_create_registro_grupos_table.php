dbc07a708b08d9f2fabde12b2c5a37b18643d364
JoDemVel <josuedemetriovelizescobar@gmail.com>
2022-06-19 11:05:06 -0400

    añadir migracion de tabla registro grupos

---
+++ database/migrations/2022_06_19_144712_create_registro_grupos_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistroGruposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registro_grupos', function (Blueprint $table) {
+            $table->id('id_reg_grupos');
+            $table->unsignedBigInteger('id_admin')->nullable();
+            $table
+                ->foreign('id_admin')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+            $table->unsignedBigInteger('id_docente')->nullable();
+            $table
+                ->foreign('id_docente')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+            $table->unsignedBigInteger('id_materia')->nullable();
+            $table
+                ->foreign('id_materia')
+                ->references('id_materia')
+                ->on('materias')
+                ->onDelete('set null');
+            $table->unsignedBigInteger('id_grupo')->nullable();
+            $table
+                ->foreign('id_grupo')
+                ->references('id_grupo')
+                ->on('grupos')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registro_grupos');
+    }
+}
